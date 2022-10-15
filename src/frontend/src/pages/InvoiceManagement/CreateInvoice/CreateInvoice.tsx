58e1220a50fbfb3778356602ec7ceb899774b260
Abdul Rehman <mehars.6925@gmail.com>
2022-10-16 02:28:14 +0500

    created an invoice page

---
+++ src/frontend/src/pages/InvoiceManagement/CreateInvoice/CreateInvoice.tsx
@@ -0,0 +1,13 @@
+import { Component, ReactNode } from "react";
+
+export default class CreateInvoice extends Component<any, any> {
+
+  render(): ReactNode {
+    return (
+      <div>
+        Create Invoice Form goes here...
+      </div>
+    )
+  }
+
+}
