25194ec040a8749cec4c477beee4f05b8c9cdab6
Abdul Rehman <mehars.6925@gmail.com>
2022-10-22 17:21:21 +0500

    disabled pagination as of now

--- src/frontend/src/pages/InvoiceManagement/Components/Invoice/Invoice.tsx
+++ src/frontend/src/pages/InvoiceManagement/Components/Invoice/Invoice.tsx
@@ -90,6 +90,7 @@ export default class Invoice extends Component<InvoiceProps, InvoiceState> {
         <Table 
           loading={isLoading} 
           columns={columns} 
+          pagination={false}
           dataSource={invoices.map(
             ({ id, customerName, customerPhone, productName, productPrice, productMeasurements, productQuantity, payments, netPayable }, index) => ({
               key: id,
