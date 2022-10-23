8a48f0a4db48ec157c7cb6ff93c215cba9438a36
Abdul Rehman <mehars.6925@gmail.com>
2022-10-23 19:00:16 +0500

    switched to hashrouter

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { Component } from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import Dashboard from "./layouts/Dashboard";
 import store from "./store";
@@ -8,13 +8,13 @@ import store from "./store";
 export default class App extends Component<any, any> {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Provider store={store}>
           <Dashboard>
             <AppRoutes />
           </Dashboard>
         </Provider>
-      </BrowserRouter>
+      </HashRouter>
     )
   }
 }
